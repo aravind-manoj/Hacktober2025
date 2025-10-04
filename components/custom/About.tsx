2356d4c9694afd8f9a7a20b9e33ad23718876bac
Abin Thomas <followgamer102@gmail.com>
2025-10-04 22:28:48 +0530

    feat(ui): About section added

---
+++ components/custom/About.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutSection: React.FC = () => {
+  return (
+    <section id="about" className="w-full">
+      <img
+        src="https://api.builder.io/api/v1/image/assets/TEMP/b139e8b545b33360dc1cf4746b93801291f04824?width=3456"
+        alt="About Hacktober - Halloween themed hackathon event details and information"
+        className="w-full h-[1119px] block object-cover"
+      />
+    </section>
+  );
+};
+
+export default AboutSection;
